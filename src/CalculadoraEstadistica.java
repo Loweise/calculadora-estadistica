a98901fd4f5f341ffbabfbf30037079c69fb1d2c
Zahid Perez <angelperezap711@gmail.com>
2026-08-18 15:09:06 -0700

    Agregar cálculo de desviación estándar

--- src/CalculadoraEstadistica.java
+++ src/CalculadoraEstadistica.java
@@ -8,8 +8,20 @@ public class CalculadoraEstadistica{
         return suma / datos.length;
     }
 
+    public static double calcularDesviacionEstandar(double[] datos){
+        double media = calcularMedia(datos);
+        double suma = 0;
+
+        for(double dato: datos){
+            suma += Math.pow(dato - media, 2);
+        }
+        return Math.sqrt(suma / datos.length);
+    }
+
     public static void main(String[] args){
         double[] datos={10,20,30,40,50};
         System.out.printf("Media = %.2f%n", calcularMedia(datos));
+
+        System.out.printf("Desviacion estándar = %.2f%n", calcularDesviacionEstandar(datos));
     }
 }
