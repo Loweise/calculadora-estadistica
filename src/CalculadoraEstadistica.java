e9aab100c76a782a166834780700a66d9f68ff98
Zahid Perez <angelperezap711@gmail.com>
2026-08-18 13:23:18 -0700

    Mejorar formato de salida

--- src/CalculadoraEstadistica.java
+++ src/CalculadoraEstadistica.java
@@ -10,6 +10,6 @@ public class CalculadoraEstadistica{
 
     public static void main(String[] args){
         double[] datos={10,20,30,40,50};
-        System.out.println("Media ="+calcularMedia(datos));
+        System.out.printf("Media = %.2f%n", calcularMedia(datos));
     }
 }
